1c76cac6cf2e4b0976891a8a47c11dfdd382b67d
Idess Lee <idesslee@qualtrics.com>
2021-07-12 16:32:49 -0700

    number pad

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import star from "./star.png";
 class StarWindow extends React.Component<any, any> {
     render() {
         return (
-            <div>{
-                [...Array(this.props.numStars)].map(() => (
+            <div>
+                {[...Array(this.props.numStars)].map(() => (
                         <img alt="star" src={star} />
-                ))
-            }</div>
+                ))}
+            </div>
         );
     }
 }
@@ -18,9 +18,27 @@ class NumberWindow extends React.Component<any, any> {
     render() {
         return (
             <div>
-                Numbers
+                <ul>
+                    <Number value={1} />
+                    <Number value={2} />
+                    <Number value={3} />
+                    <Number value={4} />
+                    <Number value={5} />
+                    <Number value={6} />
+                    <Number value={7} />
+                    <Number value={8} />
+                    <Number value={9} />
+                </ul>
             </div>
         );
+    }
+}
+
+class Number extends React.Component<any, any> {
+    render() {
+        return (
+            <li>{this.props.value}</li>
+        )
     }
 }
 
